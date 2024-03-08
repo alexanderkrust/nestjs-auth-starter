715563c0e773fb032d22b05b69e060c1e8fe48e9
Alexander Rust <alexander.rust@vodafone.com>
2024-03-08 08:03:40 +0100

    feat: add cors and fastify adapter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(9003);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+  );
+  app.enableCors({
+    origin: process.env.APP_ORIGIN || '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+  await app.listen(process.env.APP_PORT || 3001);
 }
 bootstrap();
